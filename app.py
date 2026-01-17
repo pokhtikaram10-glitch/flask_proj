86b53dc2b1443161c93d9d6169fa9a43512ce62e
unknown <pokhtikaram10@gmail.com>
2026-01-17 16:10:37 +0545

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+subjects = ['maths','social','english','population']
+
+@app.route("/")
+def index():
+    return render_template('index.html',name='tikaram',subjects = subjects)
+
+@app.route("/about")
+def about():
+    return "my name is tikaram"
+
+if __name__ == "__main__":
+    app.run(debug=True)
